d3e3349301ae131c050cce5ad013ffd520ce63a2
francisco <franciscolira408@gmail.com>
2023-02-01 00:43:49 -0800

    Added Manager

---
+++ Managers/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  Weather
+//
+//  Created by Francisco Lira on 2/1/23.
+//
+
+import Foundation
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Francisco Lira on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
