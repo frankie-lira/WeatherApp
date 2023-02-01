a8bdabfec4b08fb06f55f40a91fdc78647c77827
francisco <franciscolira408@gmail.com>
2023-02-01 01:11:05 -0800

    Created Location Manager

--- Managers/LocationManager.swift
+++ Managers/LocationManager.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate { //had to include three protocols
+    let manager = CLLocationManager() //new instantce of  location manager
+    
+    @Published var location: CLLocationCoordinate2D? //its optional since user may not grant access
+    @Published var isLoading = false
+    
+    override init() { //when our class is initialize we have to call this
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation(){
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) { //
+        location = locations.first?.coordinate //
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error getting location", error)
+        isLoading = false
+    }
+}
+
