85cc44a92fa315a9e3d9b4a63e537a9217105898
francisco <franciscolira408@gmail.com>
2023-02-01 23:03:07 -0800

    fetched weather data and created loading page view

---
+++ Views/WeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherView.swift
+//  Weather
+//
+//  Created by Francisco Lira on 2/1/23.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
